80389c9bbda7252f12fc1d80315d53a289f7f67e
Zebraholic <rctavares@rogers.com>
2014-10-22 17:11:58 -0400

    Removed salmon from list.

--- exercise6.rb
+++ exercise6.rb
@@ -1,12 +1,12 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-grocery_list.each do |item|
+	grocery_list.each do |item|
 	puts "* #{item}"
-end	
+	end	
 puts
 grocery_list << "rice"
-grocery_list.each do |item|
+	grocery_list.each do |item|
 	puts "* #{item}"
-end	
+	end	
 puts
 puts "You have a total of #{grocery_list.count} items on your grocery list."
 puts
@@ -22,5 +22,12 @@ puts
   grocery_list.each do |item|
   puts "* #{item}"
   end
-
+  puts
+  grocery_list.delete("salmon")
+  puts grocery_list
+puts
+  grocery_list = grocery_list.sort
+  grocery_list.each do |item|
+  puts "* #{item}"
+  end
 
