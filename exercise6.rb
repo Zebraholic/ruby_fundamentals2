e4ececba1b6a9f3752f572396521c0d1a8c1f1c3
Zebraholic <rctavares@rogers.com>
2014-10-22 16:41:23 -0400

    Added check for bananas.

--- exercise6.rb
+++ exercise6.rb
@@ -9,3 +9,12 @@ grocery_list.each do |item|
 end	
 puts
 puts "You have a total of #{grocery_list.count} items on your grocery list."
+puts
+  if grocery_list.include?("bananas") == false
+	puts "You don't need to pick up bananas today."
+  else
+  	puts "You need to pick up bananas."
+  end
+
+
+
